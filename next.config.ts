fd9337da894a8e7f3358b126c326407d7bc5ea3f
NeilRow56 <neilrowland56@gmail.com>
2025-11-07 06:53:45 +0000

    first commit - nextjs installed and shadcn

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "w0mlmrgwbziwquaq.public.blob.vercel-storage.com",
+      },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "static.vecteezy.com" },
+      { protocol: "https", hostname: "avatar.vercel.sh" },
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+    ],
+  },
 };
 
 export default nextConfig;
