bea0470a7455f1d841f1fa1079c1501c2cafbc1f
NeilRow56 <neilrowland56@gmail.com>
2025-11-12 15:23:26 +0000

    Second stage of individual template page

--- src/app/(admin)/dashboard/templates/[slug]/page.tsx
+++ src/app/(admin)/dashboard/templates/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useUser } from "@/lib/client-session";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCallback } from "react";
 import ReactFlow, {
   MiniMap,
@@ -16,9 +16,13 @@ import ReactFlow, {
 } from "reactflow";
 
 import "reactflow/dist/style.css";
+import { mockTemplates } from "@/lib/mock";
+import { toast } from "sonner";
+import { Fira_Sans_Extra_Condensed } from "next/font/google";
 
 export default function TemplateDetailsPage() {
   const params = useParams();
+  const slug = params.slug as string;
 
   const session = useUser();
   const user = session?.data?.user;
@@ -26,6 +30,30 @@ export default function TemplateDetailsPage() {
   const [selectNode, setSelectedNode] = useState<Node | null>(null);
   const [nodes, setNodes] = useNodesState([]);
   const [edges, setEdges] = useEdgesState([]);
+  const [template, setTemplate] = useState<any>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalNodeData, setModalNodeData] = useState<any>(null);
 
-  return <div> Template details page for {user?.name}</div>;
+  useEffect(() => {
+    const foundTemplate = mockTemplates.find((t) => t.id === slug);
+    if (!foundTemplate) return;
+    setTemplate(foundTemplate);
+  }, [slug]);
+
+  return (
+    <div className="flex h-full">
+      {/* Left Column Header and canvas*/}
+      <div className="flex-1 p-6 flex flex-col h-full ">
+        <div className="flex items-center justify-between mb-6">
+          <div>
+            <h1>Edit Template: {template?.name || slug}</h1>
+            <p>
+              {template?.description ||
+                "Design your workflow by connecting nodes"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
